1d064f41158178c51a18f21285c90ba330a9094e
thiago <thiago_gja27@hotmail.com>
2024-12-10 03:37:48 -0300

    Configurar login.html como página inicial

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 // Middleware para JSON
 app.use(express.json());
 
-// Rotasconst chegadasRouter = require('./routes/chegadas');
-const movimentacoesRoutes = require ('./routes/movimentacoes');
+// Importação de rotas
+const movimentacoesRoutes = require('./routes/movimentacoes');
 const usuariosRoutes = require('./routes/usuarios');
-const naviosRoutes = require('./routes/navios')
+const naviosRoutes = require('./routes/navios');
 const capacidadesRoutes = require('./routes/capacidades');
 const indicadoresRoutes = require('./routes/indicadores');
 const authRoutes = require('./routes/auth');
-const path = require('path');
 const posicoesRoutes = require('./routes/posicoes');
 const chegadasRouter = require('./routes/chegadas');
 
 // Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
+// Rotas da API
 app.use('/movimentacoes', movimentacoesRoutes);
 app.use('/usuarios', usuariosRoutes);
 app.use('/navios', naviosRoutes);
@@ -29,23 +27,20 @@ app.use('/capacidades', capacidadesRoutes);
 app.use('/indicadores', indicadoresRoutes);
 app.use('/auth', authRoutes);
 app.use('/posicoes', posicoesRoutes);
-
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/chegadas', chegadasRouter);
 
-
-// Rota inicial
+// Rotas principais para servir páginas HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
-  });
+});
 
-  app.get('/insert', (req, res) => {
+app.get('/insert', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'insert.html'));
-  });
+});
+
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
-
 
 // Iniciar o servidor
 app.listen(port, () => {
